1cc8bcba7bb54b19407bc68c4cc002695c084907
Matthew Hardcastle <mhtenshokumail@gmail.com>
2026-07-27 21:27:37 +0900

    updated about me page and added local storage to dark mode

--- script.js
+++ script.js
@@ -2,20 +2,24 @@ const toggleBtn = document.querySelector('.js-toggle-btn');
 const aboutBox = document.querySelectorAll(".js-about-box");
 const linkLogo = document.querySelectorAll('.js-link-logo');
 
-toggleBtn.addEventListener('click', () => {
-  if (!document.body.classList.contains('dark-mode')) {
+
+const savedTheme = localStorage.getItem('theme');
+const darkMode = (savedTheme === 'dark');
+
+
+if (darkMode) {
     document.body.classList.add('dark-mode');
     toggleBtn.innerText = "☀️";
-
     aboutBox.forEach(box => box.classList.add('about-dark'));
-
     linkLogo.forEach(logo => logo.classList.add('link-dark'));
-  } else {
-    document.body.classList.remove('dark-mode');
-    toggleBtn.innerText = "🌙";
+}
 
-    aboutBox.forEach(box => box.classList.remove('about-dark'));
+toggleBtn.addEventListener('click', () => {
+  const darkTheme = document.body.classList.toggle('dark-mode');
 
-    linkLogo.forEach(logo => logo.classList.remove('link-dark'));
-  }
+  localStorage.setItem('theme', darkTheme ? "dark" : "light");
+
+  toggleBtn.textContent = darkTheme ? "☀️" : "🌙"
+  aboutBox.forEach(box => box.classList.add('about-dark', darkTheme));
+  linkLogo.forEach(logo => logo.classList.add('link-dark', darkTheme));
 });
