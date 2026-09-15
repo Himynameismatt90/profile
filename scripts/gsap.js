2b0af3921a9e5f0780967adf610ce6f3b962e61b
Matthew Hardcastle <mhtenshokumail@gmail.com>
2026-09-15 19:07:27 +0900

    added scrollto features

--- scripts/gsap.js
+++ scripts/gsap.js
@@ -16,6 +16,27 @@ gsap.to(".temp-container", {
   ScrollTrigger: ".temp-container",
   x: 80,
   duration: 2
-})
+});
 
-gsap.to(".projects-wrapper", { duration: 2, scrollTo: 400 });
+// scrollTo section
+
+const topButton = document.getElementById("js-top-btn");
+const aboutButton = document.getElementById("js-about-btn");
+const projectsButton = document.getElementById("js-projects-btn");
+const contactButton = document.getElementById("js-contact-btn");
+
+topButton.addEventListener('click', () => {
+  gsap.to(window, { duration: 1, scrollTo: {y:".navbar"} });
+});
+
+aboutButton.addEventListener('click', () => {
+  gsap.to(window, { duration: 1, scrollTo: {y:".about-me-desc"}});
+});
+
+projectsButton.addEventListener('click', () => {
+  gsap.to(window, { duration: 1, scrollTo: {y:".title", offsetY: 120}});
+});
+
+contactButton.addEventListener('click', () => {
+  gsap.to(window, { duration:1, scrollTo: {y:".link-box"} })
+})
